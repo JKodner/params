9edbe3f8c6d23bf491880119e27467fc7663612e
Jacob Kodner <jake@kodner.net>
2014-04-07 09:30:06 -0700

    Added Docstring and added exception handlers

--- params.py
+++ params.py
@@ -1,4 +1,8 @@
 class params(object):
+	"""Params Class:
+
+	This class is a collection of parameters that can be reused for any function. Any type
+	can be inputted."""
 	def __init__(self, *args): 
 		new_dict = {}
 		for x, y in enumerate(args):
@@ -6,5 +10,9 @@ class params(object):
 			new_dict.update({name: y})
 		self.__dict__ = new_dict
 	def call(self, func):
+		"""Calls a function, with the function's parameters being the object's __dict__ values."""
 		args = tuple(self.__dict__.values())
-		return eval("func" + str(args))
+		try:
+			return eval("func" + str(args))
+		except Exception, e:
+			raise ValueError("Given Function is not valid for calling: %s" % e)
